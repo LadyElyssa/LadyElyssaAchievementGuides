75ae68538bcba3f5c64bd2cb874bf63ace50a3f5
Lady Elyssa <111598815+LadyElyssa@users.noreply.github.com>
2024-07-19 00:14:44 +0100

    Quick Zip for BlishHUD

--- !zip Blish.bat
+++ !zip Blish.bat
@@ -1,13 +1,13 @@
-@IF EXIST "LadyElyssa_FFaF.zip" del \Q "LadyElyssa_FFaF.zip"
-@IF EXIST "LadyElyssa_FFaF.taco" goto Exists
-@powershell Compress-Archive "'Data\'","*.xml" LadyElyssa_FFaF.zip -Force
-@ren LadyElyssa_FFaF.zip LadyElyssa_FFaF.taco
-@MOVE /Y "LadyElyssa_FFaF.taco" "E:\Documents\Guild Wars 2\addons\blishhud\markers"
+@IF EXIST "LadyElyssa_AP.zip" del \Q "LadyElyssaAP.zip"
+@IF EXIST "LadyElyssaAP.taco" goto Exists
+@powershell Compress-Archive "'Data\'","*.xml" LadyElyssaAP.zip -Force
+@ren LadyElyssaAP.zip LadyElyssaAP.taco
+@MOVE /Y "LadyElyssaAP.taco" "E:\Documents\Guild Wars 2\addons\blishhud\markers"
 goto Done
 
 :Exists
-del \Q "LadyElyssa_FFaF.taco"
+del \Q "LadyElyssaAP.taco"
 
 :Done
-@MOVE /Y "LadyElyssa_FFaF.taco" "E:\Documents\Guild Wars 2\addons\blishhud\markers"
+@MOVE /Y "LadyElyssaAP.taco" "E:\Documents\Guild Wars 2\addons\blishhud\markers"
 exit
