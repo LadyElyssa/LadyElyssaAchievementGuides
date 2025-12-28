1f6206c92375ec20c2514666786364034797e794
Lady Elyssa <111598815+LadyElyssa@users.noreply.github.com>
2025-12-28 20:09:42 +0000

    xmlchk.py

---
+++ xmlchk.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Download Python from: https://www.python.org/downloads/windows/
+Further Info: https://wiki.python.org/moin/BeginnersGuide/Download
+
+Usage: python xmlchk.py
+"""
+
+import pathlib
+import xml.etree.ElementTree as ET
+
+def check_file(path: pathlib.Path):
+    try:
+        ET.parse(path)
+    except Exception as e:
+        print(f"{path.name}: FAILED")
+        print(f"{type(e).__module__}.{type(e).__name__}: {e}\n")
+    """
+    else:
+        print(f"{path.name}: OK")
+    """
+
+def main():
+    p = pathlib.Path('.')
+    xml_files = sorted(p.glob('*.xml'))
+    for f in xml_files:
+        check_file(f)
+    print(f"\nXML Check Completed Successfully! ")
+
+if __name__ == "__main__":
+    main()
